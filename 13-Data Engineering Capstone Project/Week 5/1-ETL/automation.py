5e7018bad874e9de5ab61b79c062d14f974a9fb1
Fatih Somer <40704702+fatihsomer@users.noreply.github.com>
2023-09-14 10:14:42 +0200

    Add files via upload

--- 13-Data Engineering Capstone Project/Week 5/1-ETL/automation.py
+++ 13-Data Engineering Capstone Project/Week 5/1-ETL/automation.py
@@ -1,16 +1,53 @@
 # Import libraries required for connecting to mysql
+import mysql.connector
 
-# Import libraries required for connecting to DB2 or PostgreSql
+# Import libraries required for connecting to DB2 (or PostgreSql)
+import ibm_db
 
 # Connect to MySQL
+connection = mysql.connector.connect(user='root', password='xxxxxxx',host='127.0.0.1',database='sales')
+if (connection):
+	print("Connected to MySQL")
 
-# Connect to DB2 or PostgreSql
+cursor = connection.cursor()
 
-# Find out the last rowid from DB2 data warehouse or PostgreSql data warehouse
+# Connect to DB2  (or PostgreSql)
+dsn_hostname = "xxx.databases.appdomain.cloud" # e.g.: "dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net"
+dsn_uid = "xyz"        # e.g. "abc12345"
+dsn_pwd = "abc"      # e.g. "7dBZ3wWt9XN6$o0J"
+dsn_port = "00000"                # e.g. "50000" 
+dsn_database = "bludb"            # i.e. "BLUDB"
+dsn_driver = "{IBM DB2 ODBC DRIVER}" # i.e. "{IBM DB2 ODBC DRIVER}"           
+dsn_protocol = "TCPIP"            # i.e. "TCPIP"
+dsn_security = "SSL"              # i.e. "SSL"
+
+#Create the dsn connection string
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};").format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid, dsn_pwd, dsn_security)
+
+# create connection
+conn = ibm_db.connect(dsn, "", "")
+if (conn):
+    print ("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+
+# Find out the last rowid from DB2 data warehouse (or PostgreSql) data warehouse
 # The function get_last_rowid must return the last rowid of the table sales_data on the IBM DB2 database or PostgreSql.
 
 def get_last_rowid():
-	pass
+	SQL = "SELECT MAX(rowid) FROM sales_data"
+	stmt = ibm_db.exec_immediate(conn, SQL)
+	tuple = ibm_db.fetch_tuple(stmt)
+	if tuple:
+		row_id = tuple[0]
+		return row_id
+	return False
 
 
 last_row_id = get_last_rowid()
@@ -20,23 +57,35 @@ print("Last row id on production datawarehouse = ", last_row_id)
 # The function get_latest_records must return a list of all records that have a rowid greater than the last_row_id in the sales_data table in the sales database on the MySQL staging data warehouse.
 
 def get_latest_records(rowid):
-	pass	
+	if rowid:
+		SQL = "SELECT * FROM sales_data WHERE rowid >" + str(rowid)
+	else:
+		SQL = "SELECT * FROM sales_data"
+	cursor.execute(SQL)
+	records = cursor.fetchall()
+	return records	
 
 new_records = get_latest_records(last_row_id)
 
 print("New rows on staging datawarehouse = ", len(new_records))
 
-# Insert the additional records from MySQL into DB2 or PostgreSql data warehouse.
+# Insert the additional records from MySQL into DB2 (or PostgreSql) data warehouse.
 # The function insert_records must insert all the records passed to it into the sales_data table in IBM DB2 database or PostgreSql.
 
 def insert_records(records):
-	pass
+	if len(records) > 0:
+		SQL = "INSERT INTO sales_data(rowid, product_id, customer_id, quantity) VALUES(?,?,?,?);"
+		stmt = ibm_db.prepare(conn, SQL)
+		for row in records:
+			ibm_db.execute(stmt, row)
 
 insert_records(new_records)
 print("New rows inserted into production datawarehouse = ", len(new_records))
 
 # disconnect from mysql warehouse
+connection.close()
 
-# disconnect from DB2 or PostgreSql data warehouse 
+# disconnect from DB2 (or PostgreSql) data warehouse 
+ibm_db.close(conn)
 
 # End of program
